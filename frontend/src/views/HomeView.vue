4919672a72ccfc57aa336fe6854c9d6be7369292
sandiprb <baradiya.sandip@gmail.com>
2023-01-06 14:28:12 +0530

    location access working

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <button @click="getLocation">Get Location</button>
+    <pre v-if="geoData.coords">
+      latitude: {{ geoData.coords.latitude }}
+      longitude: {{ geoData.coords.longitude }}
+      accuracy: {{ geoData.coords.accuracy }}
+      altitude: {{ geoData.coords.altitude }}
+      altitudeAccuracy: {{ geoData.coords.altitudeAccuracy }}
+      heading: {{ geoData.coords.heading }}
+      speed: {{ geoData.coords.speed }}
+    </pre>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script setup lang="ts">
+import { defineComponent, reactive, watch } from 'vue';
+import { useGeolocation } from '@vueuse/core'
 
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+const geoData = reactive({
+  coords: null,
+  locatedAt: null,
+  error: null,
+  resume: null,
+  pause: null,
+})
+const getLocation = () => {
+  const { coords, locatedAt, error, resume, pause } = useGeolocation()
+
+  Object.assign(geoData, { coords, locatedAt, error, resume, pause })
+}
+
+watch(geoData, (val) => {
+  console.log(val.coords)
+  if(val.error?.code === 1){
+    alert('Please allow location access to continue!')
+  }
+})
 </script>
